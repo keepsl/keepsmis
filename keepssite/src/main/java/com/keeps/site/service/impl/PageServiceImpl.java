08e46c0dcc494d4f9ad17e4fbd648db0cf8626da
keepsl <keepsl@163.com>
2017-01-18 15:12:01 +0800

    commit

---
+++ keepssite/src/main/java/com/keeps/site/service/impl/PageServiceImpl.java
@@ -0,0 +1,18 @@
+package com.keeps.site.service.impl;
+
+import com.keeps.site.service.PageService;
+
+/** 
+ * <p>Title: PageServiceImPL.java</p>  
+ * <p>Description: @TODO </p>  
+ * <p>Copyright: Copyright (c) KEEPS</p>  
+ * @author keeps
+ * @version v 1.00
+ * @date 创建日期：2017年1月18日
+ * 修改日期：
+ * 修改人：
+ * 复审人：
+ */
+public class PageServiceImpl implements PageService {
+
+}
