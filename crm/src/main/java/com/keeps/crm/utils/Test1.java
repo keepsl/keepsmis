897f74672ca32269700fca73f06788804a039613
keepsl <keepsl@163.com>
2017-07-17 16:49:35 +0800

    test

---
+++ crm/src/main/java/com/keeps/crm/utils/Test1.java
@@ -0,0 +1,20 @@
+package com.keeps.crm.utils;
+/** 
+ * <p>Title: Test1.java</p>  
+ * <p>Description: @TODO </p>  
+ * <p>Copyright: Copyright (c) KEEPS</p>  
+ * @author keeps
+ * @version v 1.00
+ * @date 创建日期：2017年7月17日
+ * 修改日期：
+ * 修改人：
+ * 复审人：
+ */
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
